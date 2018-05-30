1aadaf81a2ec6045be70581f69efc7be592b32a8
geneyuriy <yuriy@MacBook.lan>
2018-05-31 00:17:12 +0800

    add babel-loader

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -10,7 +10,13 @@ module.exports = {
 		rules: [{
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader']
-		}]
+		},{
+			test: /\.js$/,
+			use: 'babel-loader',
+			include: path.resolve(__dirname, '../'),
+			exclude: /node_modules/
+		}
+		]
 	},
 	plugins: [
 		new cleanWebpackPlugin(['dist']),
